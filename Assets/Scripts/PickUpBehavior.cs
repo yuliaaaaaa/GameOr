98864a0459499835ca9e635d5aee59168bc18450
yuliaaaaaa <yyyyulia.04@gmail.com>
2024-03-23 16:39:20 +0200

    7 commit

--- Assets/Scripts/PickUpBehavior.cs
+++ Assets/Scripts/PickUpBehavior.cs
@@ -1,21 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PickUpBehavior : MonoBehaviour
 {
-    private bool outLine = false;
+    public Outline script;
 
     private void Start()
     {
-        
+        script.enabled = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            script.enabled = true;
 
         }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            
+        }     
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            script.enabled = false;
+        }
+    }
+
+    private void GetItem()
+    {
+        Destroy(gameObject);
     }
 }
